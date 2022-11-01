5ceba2230346e768216a858699064ce45c2e20c5
DESKTOP-J0MJJ09\Admin <ijazahmadabbas@gmail.com>
2022-11-02 02:35:13 +0500

    Plished Admin panel theme

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+    <link rel="stylesheet" href="{{ asset('assets/css/polished.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/iconic/css/open-iconic-bootstrap.min.css') }}">
+
+    <style>
+        .grid-highlight {
+            padding-top: 1rem;
+            padding-bottom: 1rem;
+            background-color: #5c6ac4;
+            border: 1px solid #202e78;
+            color: #fff;
+        }
+
+        hr {
+            margin: 6rem 0;
+        }
+
+        hr+.display-3,
+        hr+.display-2+.display-3 {
+            margin-bottom: 2rem;
+        }
+    </style>
+    <script type="text/javascript">
+        document.documentElement.className = document.documentElement.className.replace('no-js', 'js') + (document
+            .implementation.hasFeature("http://www.w3.org/TR/SVG11/feature#BasicStructure", "1.1") ? ' svg' : ' no-svg');
+    </script>
+    <script>
+        ! function(f, b, e, v, n, t, s) {
+            if (f.fbq) return;
+            n = f.fbq = function() {
+                n.callMethod ?
+                    n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+            };
+            if (!f._fbq) f._fbq = n;
+            n.push = n;
+            n.loaded = !0;
+            n.version = '2.0';
+            n.queue = [];
+            t = b.createElement(e);
+            t.async = !0;
+            t.src = v;
+            s = b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t, s)
+        }(window, document, 'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '564839313686027');
+        fbq('track', 'PageView');
+    </script>
+    <noscript><img height="1" width="1" style="display:none"
+            src="https://www.facebook.com/tr?id=564839313686027&ev=PageView&noscript=1" /></noscript>
+    @stack('styles')
+</head>
+
+<body>
+    @include('layouts.header')
+    <div class="container-fluid h-100 p-0">
+        <div style="min-height: 100%" class="flex-row d-flex align-items-stretch m-0">
+            @include('layouts.sidebar')
+            <div class="col-lg-10 col-md-9">
+                @yield('content')
+            </div>
+        </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
+    @stack('scripts')
+</body>
+
+</html>
---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,34 @@
+<div class="polished-sidebar bg-light col-12 col-md-3 col-lg-2 p-0 collapse d-md-inline" id="sidebar-nav">
+
+    <ul class="polished-sidebar-menu ml-0 pt-4 p-0 d-md-block">
+        <input class="border-dark form-control d-block d-md-none mb-4" type="text" placeholder="Search"
+            aria-label="Search" />
+        <li class="active"><a href="{{ url('/') }}"><span class="oi oi-home"></span> Home</a></li>
+        <li><a href="dashboard.html"><span class="oi oi-dashboard"></span> Dashboard</a></li>
+        <li><a href="charts.html"><span class="oi oi-pie-chart"></span> Charts</a></li>
+        <li><a href="widgets.html"><span class="oi oi-puzzle-piece"></span></span> Widget / UI</a></li>
+        <li><a href="forms.html"><span class="oi oi-browser"></span> Forms</a></li>
+        <li><a href="buttons.html"><span class="oi oi-plus"></span> Buttons</a></li>
+        <li><a href="table.html"><span class="oi oi-spreadsheet"></span> Table</a></li>
+        <li><a href="colors.html"><span class="oi oi-sun"></span> Colors</a></li>
+        <li><a href="font-sizes.html"><span class="oi oi-text"></span> Font Sizes</a></li>
+        <div class="pt-4">
+            <a href="#" class="pl-3 fs-smallest fw-bold text-muted">LAYOUT OPTIONS </a>
+            <ul class="list-unstyled">
+                <li class=""><a href="two-columns.html"><span class="oi oi-vertical-align-top"></span>Two
+                        Columns</a></li>
+                <li class=""><a href="one-column.html"><span class="oi oi-monitor"></span>One
+                        Column</a></li>
+            </ul>
+        </div>
+        <div class="d-block d-md-none">
+            <div class="dropdown-divider"></div>
+            <li><a href="#"> Profile</a></li>
+            <li><a href="#"> Setting</a></li>
+            <li><a href="#"> Sign Out</a></li>
+        </div>
+    </ul>
+    <div class="pl-3 d-none d-md-block position-fixed" style="bottom: 0px">
+        <span class="oi oi-cog"></span> Setting
+    </div>
+</div>
